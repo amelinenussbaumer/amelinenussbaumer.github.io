3a7997364f457f7029e69f8c77b3b230d9ffaf57
Raphaël Nussbaumer <rafnuss@gmail.com>
2024-09-19 16:56:32 +0200

    fix watch issue in production

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,68 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const TerserJSPlugin = require('terser-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserJSPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-module.exports = {
-  entry: './src/index.js',
-  // https://webpack.js.org/plugins/mini-css-extract-plugin/#minimizing-for-production
-  watch: true,
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader'
-        },
-        {
-          loader: 'sass-loader',
-          options: {
-            sourceMap: true,
-            // options...
-          }
-        },
-      ]
+module.exports = (env, argv) => {
+  console.log(`Webpack Mode: ${argv.mode}`); // Directly log the mode
+
+  const isDevMode = argv.mode === "development";
+
+  return {
+    entry: "./src/index.js",
+    // https://webpack.js.org/plugins/mini-css-extract-plugin/#minimizing-for-production
+    watch: isDevMode,
+    stats: "verbose",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
     },
-    {
-      test: /\.(html)$/,
-      use: 'html-loader'
-    }
-    ]
-  },
-  optimization: {
-    minimizer: [
-      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-      // `...`,
-      new CssMinimizerPlugin(),
-      new TerserJSPlugin({})
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true,
+                // options...
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(html)$/,
+          use: "html-loader",
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg)$/,
+          type: "asset/resource",
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          type: "asset/resource",
+        },
+      ],
+    },
+    optimization: {
+      minimizer: [
+        `...`, // Extend existing minimizers (like `terser-webpack-plugin`)
+        new CssMinimizerPlugin(),
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "css/[name].bundle.css",
+      }),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html"),
+      }),
     ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].bundle.css'
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html")
-    })
-  ]
+  };
 };
